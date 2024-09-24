81a6027cf8dc74f775f4278b2a996e952ff98022
Isenpai9840 <94441768+Mukeshsahni9@users.noreply.github.com>
2024-09-24 20:13:58 +0530

    first commit
    this s the start of something legendary

---
+++ simple_bot/bot.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+require('dotenv').config();
+
+const port = 4000;
+const token = process.env.BOT_TOKEN || "7545219619:AAH7J1rdHyLa7Ws4gMmFABDOPeFMQLNjb8k" ;
+
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Bot is alive');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  const userInput = msg.text;
+  const messageId = msg.message_id;
+
+  // Send a reply to the user
+  await bot.sendMessage(chatId, userInput, {
+    reply_to_message_id: messageId,
+  });
+});
